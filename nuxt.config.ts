13b69abece6912a251674900cc09c496a53d8c35
Matt Maribojoc <matthewmaribojoc@gmail.com>
2024-08-16 11:27:16 -0400

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-07-30',
+  // Nuxt 4 directory structure and features
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  // Nuxt Modules
+  // https://nuxt.com/modules
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/eslint'
+  ],
+  hub: {
+    database: true,
+    kv: true,
+    blob: true,
+    cache: true,
+  },
+  nitro: {
+    experimental: {
+      // Enable Server API documentation within NuxtHub
+      openAPI: true
+    }
+  },
+  // Development
+  devtools: { enabled: true },
+})
